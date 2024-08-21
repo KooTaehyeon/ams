df142ce4944d530618c59cea030f244c72352af5
KooTaehyeon <vcro12123@naver.com>
2024-08-21 09:08:46 +0900

    [add] student

---
+++ web_sms/js/app.js
@@ -0,0 +1,11 @@
+// import { Student } from './Student.js';
+
+import { EventHandler } from './EventHandler.js';
+// alert('go');
+
+// studentRepository.addStudent(new Student(13, '최기정', 95, 85, 88));
+
+EventHandler();
+
+// eventHandler.eventRegist();
+// console.log(eventHandler, 'eventHandler');
---
+++ web_sms/js/utils/validator.js
@@ -0,0 +1,4 @@
+export const subjects = (value, text) => {
+  if (!value) return `${text} 점수를 정확히 입력하세요.`;
+  else return false;
+};
